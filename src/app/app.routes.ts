119d7d9604f7b6136258ee8d59772b1ab9effd81
Eya <eyaelgharbi889@gmail.com>
2024-11-17 16:28:42 +0100

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path : 'books',
+    loadChildren: () => import('./books/books.module').then(m => m.BooksModule)
+    },
+    {
+        path : 'users',
+        loadChildren: () => import('./users/users.module').then(m => m.UsersModule)
+    }
+];
+//pour creer un module=> ng g module nom_mod
+//bsh n9olou lehne kifh bsh tchargi les modules
---
+++ src/app/books/book.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Book } from './book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  Books=[
+    new Book(1, "Atomic Habits", "James Clear", 35),
+    new Book(2, "The slight edge", "Jeff Olsen", 30),
+    new Book(3, "Power of habits", "Charles Duhigg", 25)
+]
+  constructor() { }
+  getBooks(){
+    return this.Books;
+  }
+  
+}
