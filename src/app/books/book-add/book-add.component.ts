e5f30232d5028d6fc6afe7b7fe36180dc94034d9
Eya <eyaelgharbi889@gmail.com>
2024-11-22 17:13:18 +0100

    first-commit

--- src/app/books/book-add/book-add.component.ts
+++ src/app/books/book-add/book-add.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-book-add',
   templateUrl: './book-add.component.html',
   styleUrl: './book-add.component.css'
 })
 export class BookAddComponent {
-
+  addBook(f : NgForm){
+    console.log(f);
+  }
 }
